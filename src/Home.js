7a219f2bd6f72df9d4d86ff1a82f10ae4650063f
pavichandru-29 <scspavithra@gmail.com>
2024-03-09 03:49:38 -0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://www.gammastack.com/wp-content/uploads/2021/11/Top-6-Must-Have-Features-for-Fantasy-Sports-Platforms-min.jpg" alt="Welcome picture" />
+    </div>
+  )
+}
